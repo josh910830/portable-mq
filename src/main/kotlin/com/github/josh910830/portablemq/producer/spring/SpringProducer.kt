782db24f53d3858fde8528ad0578976df1b50635
Josh <josh910830@gmail.com>
2023-05-05 13:12:43 +0900

    feat: disable @Async

--- src/main/kotlin/com/github/josh910830/portablemq/producer/spring/SpringProducer.kt
+++ src/main/kotlin/com/github/josh910830/portablemq/producer/spring/SpringProducer.kt
@@ -6,6 +6,7 @@ import com.github.josh910830.portablemq.producer.Producer
 interface SpringProducer<T : Message> : Producer<T> {
 
     override fun produce(message: T) {
+        // TODO produce with topic to be dispatched by single @EventListener
         ApplicationEventPublisherHolder.get().publishEvent(message)
     }
 
--- src/test/kotlin/com/github/josh910830/portablemq/tests/example/ExampleSpringConsumer.kt
+++ src/test/kotlin/com/github/josh910830/portablemq/tests/example/ExampleSpringConsumer.kt
@@ -4,14 +4,12 @@ import com.github.josh910830.portablemq.consumer.aop.Consume
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.TestComponent
 import org.springframework.context.event.EventListener
-import org.springframework.scheduling.annotation.Async
 
 @TestComponent
 class ExampleSpringConsumer {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @Async
     @EventListener
     @Consume
     fun consume(exampleMessage: ExampleMessage) {
